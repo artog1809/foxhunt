023648d19691f69c607caeb275f0031ecece3f62
artog1809 <gdoq17@mail.ru>
2024-04-11 09:28:20 +0300

    [feat]: create game

---
+++ gameover.js
@@ -0,0 +1,13 @@
+const newGame = document.querySelector(".newGame");
+const main = document.querySelector(".main");
+const result = document.querySelector(".resultAttempts");
+
+result.textContent = window.localStorage.getItem('current');
+
+newGame.addEventListener('click', _ => {
+    window.location.href = "/index.html"
+})
+
+main.addEventListener('click', _ => {
+    window.location.href = "/main.html"
+})
---
+++ main.js
@@ -0,0 +1,22 @@
+const play = document.querySelector(".play")
+const rules = document.querySelector(".rules")
+const rule = document.querySelector(".rule")
+const menu = document.querySelector(".menu-container")
+
+let flag = 1;
+play.addEventListener('click', _ => {
+    window.location.href = "/index.html";
+})
+
+
+rules.addEventListener('click', _ => {
+    if(flag){
+        rule.style.display = "flex";
+        menu.style.height = "100%";
+        flag = 0;
+        return;
+    }
+    rule.style.display = "none";
+    menu.style.height = "70%";
+    flag = 1;
+})
